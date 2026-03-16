a335df95664144bc2373c564e44d5ac135706bb5
AbhinavVarma01 <vaishnaviabhi146@gmail.com>
2026-03-17 02:14:04 +0530

    BootCamp

--- Bootcamp.cpp
+++ Bootcamp.cpp
@@ -388,4 +388,45 @@ public:
 };*/
 
 
-/* 16: */
+/* 16: Spiral Matrix:
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        
+        vector<int> ans;
+        
+        int top = 0;
+        int bottom = matrix.size() - 1;
+        int left = 0;
+        int right = matrix[0].size() - 1;
+        
+        while(top <= bottom && left <= right) {
+            
+            // TOP
+            for(int i = left; i <= right; i++)
+                ans.push_back(matrix[top][i]);
+            top++;
+            
+            // RIGHT
+            for(int i = top; i <= bottom; i++)
+                ans.push_back(matrix[i][right]);
+            right--;
+            
+            // BOTTOM
+            if(top <= bottom) {
+                for(int i = right; i >= left; i--)
+                    ans.push_back(matrix[bottom][i]);
+                bottom--;
+            }
+            
+            // LEFT
+            if(left <= right) {
+                for(int i = bottom; i >= top; i--)
+                    ans.push_back(matrix[i][left]);
+                left++;
+            }
+        }
+        
+        return ans;
+    }
+}; */
