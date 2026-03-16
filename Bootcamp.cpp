b2a7d78dcc83be9e1f7ed7d39a8f42b6702841b6
AbhinavVarma01 <vaishnaviabhi146@gmail.com>
2026-03-17 02:29:39 +0530

    BootCampp

--- Bootcamp.cpp
+++ Bootcamp.cpp
@@ -162,6 +162,21 @@ vector<pair<int,int>> allPairs(vector<int>& arr, int target) {
     return ans;
 }
 
+SORTED INPUT: 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l = 0;
+        int r = numbers.size()-1;
+        while(l<r){
+            int sum = numbers[l] + numbers[r];
+            if(sum==target) return {l+1, r+1};
+            else if(sum<target) l++;
+            else r--;
+        }
+        return {};
+    }
+};
 */
 
 /* 6. Binary Search:
